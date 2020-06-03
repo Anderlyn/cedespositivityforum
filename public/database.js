0b636c2bc65e68a23b8d1d7aa44fdd3a4a1f396b
alstrash <sebastian.lopez@cedesdb.ed.cr>
2020-06-02 18:22:12 -0600

    First version of the app.

---
+++ public/database.js
@@ -0,0 +1,60 @@
+//                                               ¿Qué haces en mi código?    
+//                                 Espero lo estés viendo desde el repositorio de GitHub:
+//                                    https://github.com/alstrash/cedespositivityforum
+//⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠄⠄⠄⠄⠉⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⣼⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣸⣿⣿⡿⢸⣿⣿⣿⠛⣿⣿⣿⣿⡇⠄⠄⠄⠄⢀⣠⡴⠚⠁⣀⣤⢤⣤⣤⣬⣭⣙⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⡇⣸⣿⣿⡟⠄⣿⣿⣿⣿⣷⠄⢠⣾⣿⠿⠋⢀⣴⣫⣷⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⡂⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣷⣿⣿⣿⡇⠄⠘⣿⣿⣿⣿⣧⠄⠓⠄⠄⠄⠴⠿⣿⣿⠋⠄⢸⣿⣿⣿⣿⣿⣿⣿⠄⠄⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⡇⠄⢠⠾⠿⣿⣿⣿⣧⡀⠄⠄⠄⠄⠘⠋⢿⠄⠄⠈⢿⡏⠉⢻⢀⣿⡟⠄⠄⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⡿⢹⣿⣿⣿⣿⣿⡇⠄⢀⣤⣶⣾⣿⣿⣿⣿⣆⠄⠄⠄⠄⠄⠘⡇⠄⠄⠄⠉⠄⠚⣩⠟⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠇⢸⣿⣿⣿⣿⣿⣷⡀⣼⣿⣿⠉⢹⣿⣿⣿⣿⣷⣄⠄⠄⠄⠄⠄⣀⣄⣠⠤⠔⠊⠁⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⢸⣿⣿⣿⣿⣿⣿⣷⡌⣿⣿⠄⠈⣿⢿⣿⣿⢿⣿⣷⣤⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⠸⣿⣿⣿⣿⣿⣿⣿⣿⡄⠙⡆⠄⠘⠂⣑⣸⠄⠈⠙⠻⢿⣶⣄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠄⠠⣤⣤⠴⠋⠄⠄⠄⠄⠄⠉⠛⠿⣷⣦⣄⠄⠄⠄⠄⠄⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⠄⢹⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄⠈⠄⠄⠐⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠙⠒⠢⠄⠄⠄⢸⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⠄⠄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠄⠄⠄⠄⣶⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣿⣿⡿⠁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⠄⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄⠈⠻⠦⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⠁⠄⣿⣿⣿⣿⣿⡟⣿⣿⣿⣿⣿
+//⣇⠄⠄⠄⢹⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄⠄⠄⠄⠄⠄⠄⣀⠄⠄⢀⣀⣀⠄⠄⠄⠄⠄⢠⣿⡿⠁⠄⠄⣿⣿⣿⣿⡟⠄⢿⣿⣿⣿⡿
+//⠈⢆⠄⠄⠀⢿⡟⢿⣿⣿⣿⣿⣿⣿⣧⡀⠄⠄⠄⠄⠄⠈⠱⣎⢉⣀⣤⣭⣽⡷⠄⠄⢀⣿⡟⠁⠄⠄⠄⣿⣿⣿⡿⠄⠄⠸⢿⣿⣿⢻
+//⠄⠈⠂⠄⠄⠈⢿⡀⠻⣿⣿⣿⣿⣿⣿⣷⣄⠄⠄⠄⠄⠄⠄⢸⣿⣿⡿⠟⠉⠄⠄⠄⡾⠉⠄⠄⠄⠄⢀⣽⣿⣿⠁⠄⠄⠄⠄⠻⣿⠈
+//⠄⠄⠄⠄⠄⠄⠈⢧⠄⠘⢿⣿⣿⣿⣿⣿⣿⣷⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠋⠘⣿⡏⠄⠄⠄⠄⠄⣀⣀⡤
+//⠄⠄⠄⠄⠄⠄⠄⠈⢆⠄⠀⢻⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⠔⠋⠄⠄⠄⢻⠃⠄⠄⢀⠴⠋⠉⠀⠀
+//⠄⠄⠄⠄⠄⠄⠄⠄⠈⠆⠄⠄⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⡷⣤⡀⠄⠄⠄⠄⠄⠄⢀⣤⢾⠁⠄⠄⠄⠄⠄⠄⠄⢀⠔⠁⠄⠄⠄⣀⣤
+//⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢿⡟⠄⢻⣿⡏⢿⢻⣿⠟⠁⠄⠙⠷⣄⣀⣀⣤⠶⠋⠄⠈⢧⠄⠄⠄⠄⠄⢀⡴⠁⠄⠄⣠⣴⣿⣿⣿
+//⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡾⠄⠄⠄⠹⣷⠄⠄⠛⠄⠄⠄⠄⠄⠈⠉⠉⠄⠄⠄⠄⠄⠄⠳⡀⠄⠄⣰⠋⢀⣠⣴⣿⣿⣿⣿⣿⣿
+//⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⠄⠄⠄⠄⠄⠘⠆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢹⣤⠞⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⠞⣠⣴⣿⣿⣿⣿⣿⣿⣿
+
+let ideas = []; // Here
+async function readData(){
+    /*  Aquí sacamos todos los documentos de la colección de firestore. */
+    const  collection = await firebase.firestore().collection("news").get(); // Esperamos a que esto llegue.
+    return collection.docs.map(doc => ideas.push(doc.data())); // Cuando llega, metemos los objetos en un vector.
+}
+/* Componente de React para mostrar los mensajes. */
+class Message extends React.Component{
+    constructor(){
+        super(); // Herencia de propiedades.
+    }
+    render(){
+        /* En este render, iteramos por cada mensaje para construir cada idea. El vector llega por propiedades. */ 
+        return (
+            <div>
+                {this.props.messages.map(item => (
+                    <div className='message'>
+                        <h1>{item.Persona}</h1>
+                        <h2>{item.Fecha}</h2>
+                        <p>{item.Mensaje}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    } 
+};
+
+/* Cuando se termina el callback de la función asincrona, instanciamos el componente 
+pasándole como propiedad el vector de ideas que se sacó de la base de datos. */
+readData().then(function(){
+    ReactDOM.render(<Message messages={ideas} />, document.getElementById("ideas"));
+});
